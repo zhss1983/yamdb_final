8d6385a3a9681d8f35480bd413d83f9d3bd99b6a
SERGEY ZHAROV <zhss.83@yandex.ru>
2021-11-25 01:55:48 +0500

    26

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,7 +7,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = False
 
