448849dbf127f60a35a955f7d7030ef0e15d7a1d
SERGEY ZHAROV <zhss.83@yandex.ru>
2021-11-25 18:56:48 +0500

    33

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = False
+DEBUG = True
 
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 if SENTRY_DSN:
