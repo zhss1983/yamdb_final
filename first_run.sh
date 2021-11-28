f4255a9b1d2ca4b01b6199b1579c1c9d10cf0b2c
SERGEY ZHAROV <zhss.83@yandex.ru>
2021-11-28 20:58:04 +0500

    Второе ревью.

--- first_run.sh
+++ first_run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo docker-compose exec web python manage.py migrate
-sudo docker-compose exec web python manage.py createsuperuser
-sudo docker-compose exec web python manage.py collectstatic
-sudo docker-compose exec web python manage.py loaddata fixtures.json
+cp -r ./data/static ./data/static_backup
+rm -rf ./data/static
+docker-compose exec web python manage.py migrate
+docker-compose exec web python manage.py collectstatic
+docker-compose exec web python manage.py loaddata fixtures.json
+echo "include /data/nginx/default_first.conf;" > ./nginx/default.conf
+chmod 766 ./nginx/default.conf
+docker-compose restart
+./init-letsencrypt.sh
+echo "include /data/nginx/default_evryday.conf;" > ./nginx/default.conf
+chmod 766 ./nginx/default.conf
+docker-compose restart
